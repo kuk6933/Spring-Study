007166a3464bcecd9ca302685d4ceeec445581f3
HyeongSeok <gudtjr051911@gmail.com>
2023-05-02 13:12:55 +0900

    회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //인터페이스와 구현체에 의존. 즉 추상화에도 구체화에도 의존하는것 DIP위반
     @Override
     public void join(Member member) {
         memberRepository.save(member);
