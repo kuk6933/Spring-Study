dcaae7fa043551deb29df54498c92757baade3ce
HyeongSeok <gudtjr051911@gmail.com>
2023-05-02 12:56:37 +0900

    회원 도메인 개발

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //사실은 concurrent hashmap 써야함 동시성 이슈를 위해
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
