d0454c1e014a1eb86313bd32032a85e529ecd583
HyeongSeok <gudtjr051911@gmail.com>
2023-05-05 22:13:53 +0900

    필터

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan (  //@component 붙은애들 다 찾아서 spring bean으로 등록해줌
+        basePackages = "hello.core.member", //어디서부터 찾는지 지정, 안쓰면(default)는 해당 클래스 위치부터
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes =  Configuration.class) // AppConfig와중복되면 안되니 빼주는것
 )
 public class AutoAppConfig {
---
+++ core/src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent { //Component Scan에서 제외할 애들
+}
