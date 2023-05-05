07bfa636e14b7bfaa28cfa1f88967408ad313538
HyeongSeok <gudtjr051911@gmail.com>
2023-05-05 19:30:33 +0900

    @Configuration과 싱글톤

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -3,32 +3,44 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration //설정정봐, 구성정보
+@Configuration //설정정보, 구성정보 - 싱글톤을 위해 존재
 public class AppConfig {
-    @Bean //sPRING container에 등록하게됨
-    public static MemoryMemberRepository getMemberRepository() {
+
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository() 이렇게 두번 호출되는게 아닌가? 싱글톤이 깨지는게 아닌가?
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
-    @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(getMemberRepository()); // 생성자 주입
-    }
+
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
 }
